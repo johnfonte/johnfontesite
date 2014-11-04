b8364856affa4d9ed4b1ff5f961d305dd96fc6b1
John Fonte <john@fonte.com>
2014-11-04 12:45:31 -0500

    major overhaul
    removed bootstrap
    built custom css
    made more personal
    updated resume
    removed unused second page
    all images are backgrounds
    using font-awesome
    using media queries
    improved load time

--- app.js
+++ app.js
@@ -1,23 +1,24 @@
 var express = require('express')
   , app = express()
   , fs = require('fs')
+  , nowww = require('nowww')
   , path = require('path')
   , jade = require('jade');
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
-  app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
-  app.use(express.bodyParser());
-  app.use(express.methodOverride());
-  app.use(express.static(path.join(__dirname, 'public')));
-  app.use(express.favicon( __dirname + '/favicon.ico'));
+  app.set('port', process.env.PORT || 3000)
+  .set('views', __dirname + '/views')
+  .set('view engine', 'jade')
+  .use(nowww())
+  .use(express.json())
+  .use(express.urlencoded())
+  .use(express.methodOverride())
+  .use(express.static(path.join(__dirname, 'public')))
+  .use(express.favicon( __dirname + '/favicon.ico'));
 });
 
 var navigation = [
-  { url: "/", uri: "professional", title: "John Fonte" },
-  { url: "/hobbies", uri: "hobbies", title: "John Fonte | Hobbies" },
-  { url: "/*", uri: "404", title: "404" }
+  { url: "/*", uri: "home", title: "John Fonte" }
 ];
 
 var templates = [];
@@ -37,8 +38,6 @@ var render = function(req, res, index) {
 
 app.get(navigation[0].url, function(req, res){ render(req, res, 0); });
 
-app.get(navigation[1].url, function(req, res){ render(req, res, 1); });
-
-app.get(navigation[2].url, function(req, res){ render(req, res, 2); });
+// app.get(navigation[1].url, function(req, res){ render(req, res, 1); });
 
 app.listen(3000);
